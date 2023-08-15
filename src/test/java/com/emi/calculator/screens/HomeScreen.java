d06c1e3c8cff40406c390b7b6375b5799ea60a4c
Hiro Mia <hiromia006@gmail.com>
2023-08-15 12:19:47 +0600

    Add Home Screen Test Cases

--- src/test/java/com/emi/calculator/screens/HomeScreen.java
+++ src/test/java/com/emi/calculator/screens/HomeScreen.java
@@ -4,17 +4,28 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 
 public class HomeScreen extends BaseScreen {
+    private By btnStart = By.id("btnStart");
+    private By btnCompare = By.id("btnCompare");
+
     public HomeScreen(AndroidDriver driver) {
         super(driver);
     }
 
     public EmiCalculatorScreen tapOnEmiCalculatorBtn() {
-        getWebElement(By.id("btnStart")).click();
+        getWebElement(btnStart).click();
         return getInstance(EmiCalculatorScreen.class);
     }
 
     public CompareLoanScreen tapOnCompareLoanBtn() {
-        getWebElement(By.id("btnStart")).click();
+        getWebElement(btnCompare).click();
         return getInstance(CompareLoanScreen.class);
+    }
+
+    public boolean hasEmiCalculatorBtn() {
+        return getWebElements(btnStart).size() > 0;
+    }
+
+    public boolean hasCompareLoanBtn() {
+        return getWebElements(btnCompare).size() > 0;
     }
 }
