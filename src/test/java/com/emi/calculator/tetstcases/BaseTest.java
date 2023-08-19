40732a517b1314bc8959faaa06188561aa5f32e8
Hiro Mia <hiromia006@gmail.com>
2023-08-19 22:16:20 +0600

    implement Extend report

--- src/test/java/com/emi/calculator/tetstcases/BaseTest.java
+++ src/test/java/com/emi/calculator/tetstcases/BaseTest.java
@@ -2,13 +2,24 @@ package com.emi.calculator.tetstcases;
 
 import com.emi.calculator.screens.BaseScreen;
 import com.emi.calculator.screens.Screen;
+import com.emi.calculator.util.General;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BaseTest {
     AndroidDriver driver;
@@ -25,8 +36,11 @@ public class BaseTest {
             capabilities.setCapability("appActivity", "com.finance.emicalci.activity.Splash_screnn");
 
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver.startRecordingScreen();
 
             screen = new BaseScreen(driver);
+
+
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -35,7 +49,37 @@ public class BaseTest {
 
     @AfterClass
     public void tearDown() {
+        stopRecording();
         driver.quit();
+    }
+
+    private void stopRecording() {
+        String base64String = driver.stopRecordingScreen();
+        byte[] data = Base64.decodeBase64(base64String);
+        String destinationPath = System.getProperty("user.dir") + "/build/videos";
+        File theDir = new File(destinationPath);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+
+        String filePath = destinationPath + "/" + driver.getDeviceTime().replace(":", "_").replace("+", " ") + ".mp4";
+        System.out.println("filePath : " + filePath);
+        Path path = Paths.get(filePath);
+        try {
+            Files.write(path, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void takeScreenshot(String fileName) {
+        try {
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String currentDir = System.getProperty("user.dir") + "/build/screenshots/";
+            FileUtils.copyFile(scrFile, new File(currentDir + fileName + ".jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public AndroidDriver getWebDriver() {
